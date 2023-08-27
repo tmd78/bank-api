954c5205089b5e43ce11381b4cd210b94ec10c1c
tmd78 <toby.draper@aol.com>
2023-08-27 11:40:57 -0700

    Allow account balance to be negative

--- import-bank.sql
+++ import-bank.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS account;
 
 CREATE TABLE account (
     id INT AUTO_INCREMENT,
-    balance INT UNSIGNED NOT NULL,
+    balance INT NOT NULL,
     passcode CHAR(4) NOT NULL,
     PRIMARY KEY (id)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
@@ -21,6 +21,3 @@ CREATE TABLE transaction (
     FOREIGN KEY (account_id)
         REFERENCES account (id)
 )  ENGINE=INNODB DEFAULT CHARSET=UTF8;
-
-INSERT INTO account (id, balance, passcode) VALUES (1, 100, '1234');
-INSERT INTO transaction (account_id, action, amount) VALUES (1, 'DEPOSIT', 100);
